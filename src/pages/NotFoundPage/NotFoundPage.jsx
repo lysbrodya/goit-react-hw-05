266a4eea1acb1b72c50d4dd2efebad8a72e66dd4
lysbrodya <lysbrodya@gmail.com>
2024-03-13 23:06:19 +0200

    without src

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-export default function NotFound() {
-  return (
-    <div>
-      <h1>STALASYA POMiLKA STORINKA NE ZNAYDENA</h1>
-      <Link to="/">Back to home page</Link>
-    </div>
-  );
-}
